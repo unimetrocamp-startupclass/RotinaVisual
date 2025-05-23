f8773df062552d5d9de5208cfdc5be452cb3f506
BiancaPessinAv <142849413+BiancaPessinAv@users.noreply.github.com>
2025-05-23 15:01:53 -0300

    Header.v2 + editarAluno.v1
    Página editar aluno ainda em inicio de desenvolvimento.
    Header editado para se adequar a página conforme referência.

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,7 +1,25 @@
+import Header from '../src/components/header';
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
+    <>
+      <Header 
+        rightContent={
+          <div style={{
+            backgroundColor: '#D9F0DB',
+            borderRadius: '20px',
+            padding: '10px 15px',
+            display: 'flex',
+            alignItems: 'center',
+            gap: '8px' }}>
+            <img src="/usuario.webp" alt="Perfil" style={{ borderRadius: '50%', width: '40px', height: '38px' }}/>
+            <span>João Paulo Martins</span>
+            <img src="/Seta.webp" alt="seta" style={{ width: '20px', height: '20px' }}/>
+          </div>
+        }
+      />
+
     <main className={styles.mainDiv}>
       <div className={styles.textDiv}>
         <h1>Rotina Visual</h1>
@@ -14,5 +32,6 @@ export default function Home() {
         </p>
       </div>
     </main>
+    </>
   );
 }
---
+++ front/src/components/cardDados.tsx
@@ -0,0 +1,15 @@
+import styles from './cardDados.module.css';
+
+interface DadoPessoalProps {
+    titulo: string;
+    valor: string;
+}
+
+export default function DadoPessoal({ titulo, valor }: DadoPessoalProps) {
+  return (
+    <div className={styles.caixa}>
+      <span className={styles.titulo}>{titulo}</span>
+      <p className={styles.valor}>{valor}</p>
+    </div>
+  );
+}
