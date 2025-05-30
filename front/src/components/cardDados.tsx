164bf6c646af1f3cf7cf488eb5e8d3bb80df1fd8
AlineCarneiro <61187682+AlineCarneiro@users.noreply.github.com>
2025-05-30 17:18:20 -0300

    Update cardDados.tsx

--- front/src/components/cardDados.tsx
+++ front/src/components/cardDados.tsx
@@ -1,13 +1,14 @@
 import styles from './cardDados.module.css';
 
 interface DadoPessoalProps {
-    titulo: string;
-    valor?: string;
+  titulo: string;
+  valor: string;
+  fullWidth?: boolean;
 }
 
-export default function DadoPessoal({ titulo, valor }: DadoPessoalProps) {
+export default function DadoPessoal({ titulo, valor, fullWidth }: DadoPessoalProps) {
   return (
-    <div className={styles.caixa}>
+    <div className={`${styles.caixa} ${fullWidth ? styles.caixaCompleta : ''}`}>
       <span className={styles.titulo}>{titulo}</span>
       <p className={styles.valor}>{valor}</p>
     </div>
